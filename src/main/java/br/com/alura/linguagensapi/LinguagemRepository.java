2e41d3025063b2500cc7494cfb87942d9e8b9f6f
Bruno Paes de Medeiros <109305921+brunopaesdemedeiros@users.noreply.github.com>
2022-08-01 22:55:54 -0300

    ok

---
+++ src/main/java/br/com/alura/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,10 @@
+package br.com.alura.linguagensapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem,String> {
+
+    
+
+    
+}
